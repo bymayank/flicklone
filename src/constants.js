7c244bf85e44380005e83a771c9ba1c14ce3850d
bymayank <67113424+bymayank@users.noreply.github.com>
2021-06-26 20:04:30 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const constants = {
+	BASE_URL:
+		"https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=a07df14ca309dcaed21964fa0e500716&per_page=",
+	STORAGE_KEY: "flicker_search_queries"
+};
+
+export default constants;
